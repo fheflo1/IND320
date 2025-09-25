9ba8422de0daba9a3e5d8e141735a47b67fb2151
Fabian Heflo <fheflo@gmail.com>
2025-09-25 09:35:13 +0200

    removed dummy and cleaned up some text

--- apps/pages/1_Home.py
+++ apps/pages/1_Home.py
@@ -1,12 +1,5 @@
 import streamlit as st
 
 st.title("🏠 Home")
-st.write(
-    """
-    This is a Streamlit app for the course IND320: Data to Decisions.
-    So far, it contains interesting pages about meteorological data from Norway.
-    Use the sidebar to navigate between the pages.
-    """
-)
 
 st.info("Tip: The sidebar contains shortcuts to the other pages.")
--- apps/pages/4_Dummy.py
+++
@@ -1,4 +0,0 @@
-# import streamlit as st
-
-# st.title("🧪 Dummy page")
-# st.write("Temporary placeholder for future content.")
